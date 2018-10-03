cf42813b866489f79f8ee2ee6604175a45ed9286
matthewtoma3061 <mattttpatttt@gmail.com>
2018-10-03 12:50:25 -0400

    Scrolling Background ( Horizontal)

--- core/src/com/gotmiluk/munchingtime/MyGdxGame.java
+++ core/src/com/gotmiluk/munchingtime/MyGdxGame.java
@@ -7,27 +7,37 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
+	Texture Background1, Background2;
 	SpriteBatch batch;
-	Texture img;
-	
+	float yMax, yCoordBg1, yCoordBg2;
+	final int BACKGROUND_MOVE_SPEED = 100; // pixels per second. Put your value here.
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Background1 = new Texture(Gdx.files.internal("Background for game.jpg"));
+		Background2 = new Texture(Gdx.files.internal("Background for game.jpg")); // identical
+		yMax = 1280;
+		yCoordBg1 = yMax*(-1); yCoordBg2 = 0;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		yCoordBg1 += BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+		yCoordBg2 = yCoordBg1 + yMax;  // We move the background, not the camera
+		if (yCoordBg1 >= 0) {
+			yCoordBg1 = yMax*(-1); yCoordBg2 = 0;
+		}
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(Background1, 0, yCoordBg1);
+		batch.draw(Background2, 0, yCoordBg2);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		Background1.dispose();
+		Background2.dispose();
 	}
 }
