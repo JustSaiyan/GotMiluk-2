d8da3222f8267bd428fc780a462b94240d0b310c
Justin <goebj0742@wrdsb.ca>
2018-10-03 12:14:36 -0400

    Munching Time

---
+++ desktop/src/com/gotmiluk/munchingtime/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gotmiluk.munchingtime.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gotmiluk.munchingtime.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
