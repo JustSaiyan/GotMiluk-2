aa9c2f56c7c3564a5299e439086dd7e80c062a22
matthewtoma3061 <mattttpatttt@gmail.com>
2018-10-03 12:58:52 -0400

    Switched X and Y variables

--- core/src/com/gotmiluk/munchingtime/MyGdxGame.java
+++ core/src/com/gotmiluk/munchingtime/MyGdxGame.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	Texture Background1, Background2;
 	SpriteBatch batch;
-	float yMax, yCoordBg1, yCoordBg2;
+	float xMax, xCoordBg1, xCoordBg2;
 	final int BACKGROUND_MOVE_SPEED = 100; // pixels per second. Put your value here.
 
 	@Override
@@ -17,20 +17,20 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		Background1 = new Texture(Gdx.files.internal("Background for game.jpg"));
 		Background2 = new Texture(Gdx.files.internal("Background for game.jpg")); // identical
-		yMax = 1280;
-		yCoordBg1 = yMax*(-1); yCoordBg2 = 0;
+		xMax = 1280;
+		xCoordBg1 = xMax*(-1); xCoordBg2 = 0;
 	}
 
 	@Override
 	public void render () {
-		yCoordBg1 += BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
-		yCoordBg2 = yCoordBg1 + yMax;  // We move the background, not the camera
-		if (yCoordBg1 >= 0) {
-			yCoordBg1 = yMax*(-1); yCoordBg2 = 0;
+		xCoordBg1 += BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+		xCoordBg2 = xCoordBg1 + xMax;  // We move the background, not the camera
+		if (xCoordBg1 >= 0) {
+			xCoordBg1 = xMax*(-1); xCoordBg2 = 0;
 		}
 		batch.begin();
-		batch.draw(Background1, 0, yCoordBg1);
-		batch.draw(Background2, 0, yCoordBg2);
+		batch.draw(Background1, 0, xCoordBg1);
+		batch.draw(Background2, 0, xCoordBg2);
 		batch.end();
 	}
 
