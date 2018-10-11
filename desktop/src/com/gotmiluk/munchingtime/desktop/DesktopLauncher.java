c082c606609aa664f246ec822ead6307de238868
Justin <goebj0742@wrdsb.ca>
2018-10-11 14:15:41 -0400

    Movement Working

--- desktop/src/com/gotmiluk/munchingtime/desktop/DesktopLauncher.java
+++ desktop/src/com/gotmiluk/munchingtime/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.gotmiluk.munchingtime.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.gotmiluk.munchingtime.ScrPlay;
+import com.gotmiluk.munchingtime.GamMunch;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ScrPlay(), config);
+		new LwjglApplication(new GamMunch(), config);
 	}
 }
