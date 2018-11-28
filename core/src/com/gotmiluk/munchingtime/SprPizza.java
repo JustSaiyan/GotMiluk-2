4ae2947bf21e1ff0354785c84132c50b019dc144
matthewtoma3061 <mattttpatttt@gmail.com>
2018-11-28 12:04:20 -0500

    Working on getting the array In Animation

--- core/src/com/gotmiluk/munchingtime/SprPizza.java
+++ core/src/com/gotmiluk/munchingtime/SprPizza.java
@@ -7,50 +7,47 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.lang.reflect.Array;
+
 public class SprPizza  extends Sprite {
-    Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+
     Texture walkSheet;
-    // A variable for tracking elapsed time for the animation
-    float fstateTime;
-//    TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / 3, walkSheet.getHeight() / 1); //3 is columns 1 is rows
-
-
-
+    TextureRegion trTemp;
+   int nFrame = 0;
+   int nPos = 0;
+   Array araniDude = new Animation[8];
+   float fW = walkSheet.getWidth() / 8;
+   float fH = walkSheet.getHeight() / 8;
 
     public SprPizza(int nW, int nH, int nX, int nY) {
         walkSheet = new Texture("Pizza SpriteSheet.png");
-        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / 3, walkSheet.getHeight() / 1); //3 is columns 1 is rows
+
         setSize(nW, nH);
         setPosition(nX, nY);
         setFlip(false, false);
-        TextureRegion[] walkFrames = new TextureRegion[3 * 1];//how many frames are in texture region array;
-        int index = 0;
-        for (int i = 0; i < 1; i++) { //row #
-            for (int j = 0; j < 3; j++) { //column #
-                walkFrames[index++] = tmp[i][j];
+
+        //Animation Stuff
+
+        for (int i = 0; i < 3   ; i++) {
+            Sprite[] arSprDude = new Sprite[8];
+            for (int j = 0; j < 8; j++) {
+                fSx = j * fW;
+                fSy = i * fH;
+
+                arSprDude[j] = new Sprite(walkSheet);
             }
-        }
-        walkAnimation = new Animation<TextureRegion>(1 / 4f, walkFrames);
+            araniDude[i] = new Animation(0.8f, arSprDude);
 
-        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
-        // time to 0
 
-        fstateTime = 0f;
+
+
+
+
 
     }
-
     public void draw(Batch batch) {
 
-        fstateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
-        // Get current frame of animation for the current stateTime
-        TextureRegion currentFrame = walkAnimation.getKeyFrame(fstateTime, true);
-
-
-
-
-        batch.draw(currentFrame, getX(), getY(), 100, 100);
-        setX(getX()-1);
-    }}
+    }}}
 
 
